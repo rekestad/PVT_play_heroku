1467614ef8f962c79ade28cf548ec452ffe73b8d
Rekansson <alexander.rekestad@gmail.com>
2017-04-24 16:34:05 +0200

    ##

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -17,10 +17,10 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok("hello there!");
     }
     
-    public Result getName() {
-    	return ok("Well hello there you naughty boy!");
-    }
+    //public Result getName() {
+    	//return ok("Well hello there you naughty boy!");
+    //}
 }
