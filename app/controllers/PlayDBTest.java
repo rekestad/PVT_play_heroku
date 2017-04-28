04d20c4ccc1dc20cd78c2bd0de392b4888971d14
Felix Törnqvist <felix.trnqvist@gmail.com>
2017-04-28 14:11:39 +0200

    PreparedStatement

--- app/controllers/PlayDBTest.java
+++ app/controllers/PlayDBTest.java
@@ -6,14 +6,13 @@ import play.db.Database;
 import play.mvc.*;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * Created by felix on 2017-04-27.
  */
-public class PlayDBTest extends Controller{
+public class PlayDBTest extends Controller {
 	private Database db;
 
 	@Inject
@@ -24,64 +23,48 @@ public class PlayDBTest extends Controller{
 	public Result listUsrs() {
 		final String[] result = {""};
 
-		// my attempt to avoid all the try/catch statements (that still exist in doSQLStatement())
-		StatementProcessor sp = stmt -> {
-			ResultSet rs = stmt.executeQuery("SELECT * FROM Users");
-
-			while(rs.next()){
-				int id  = rs.getInt("User_ID");
-				int age = rs.getInt("age");
-				String fName = rs.getString("Name");
-				String desc = rs.getString("Description");
-
-				result[0] += "ID: " + id + ", age: " + age + ", name: " + fName + ", desc: " + desc + "\n";
-			}
-
-			rs.close();
-		};
-		doSQLStatement(sp);
+//		// my attempt to avoid all the try/catch statements (that still exist in doSQLStatement())
+//		StatementProcessor sp = stmt -> {
+//			ResultSet rs = stmt.executeQuery("SELECT * FROM Users");
+//
+//			while(rs.next()){
+//				int id  = rs.getInt("User_ID");
+//				int age = rs.getInt("age");
+//				String fName = rs.getString("Name");
+//				String desc = rs.getString("Description");
+//
+//				result[0] += "ID: " + id + ", age: " + age + ", name: " + fName + ", desc: " + desc + "\n";
+//			}
+//
+//			rs.close();
+//		};
+//		doSQLStatement(sp);
 
 		return ok(result[0]);
 	}
 
-	public Result makeUsr(int id, int age, String name, String desc) {
-		StatementProcessor sp = stmt -> {
-			stmt.executeUpdate("INSERT INTO Users VALUES ("+id+",\""+name+"\","+age+",\""+desc+"\")");
-		};
-		doSQLStatement(sp);
-		return listUsrs();
-	}
-
-	public void doSQLStatement(StatementProcessor sp) {
+	public Result makeUsr(int age, String name, String desc) {
 		Connection conn = db.getConnection();
-		Statement stmt = null;
-
 		try {
-			stmt = conn.createStatement();
-			sp.doQuery(stmt);
-
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Users (Name, Age, Description) VALUES (?,?,?)");
+			pstmt.setString(1, name);
+			pstmt.setString(3, desc);
+			pstmt.setInt(2, age);
+			pstmt.execute();
+			conn.commit();
+			pstmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
-
+			return ok("couldn't create user");
 		} finally {
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-
 			try {
 				conn.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-
 		}
-	}
 
-	private interface StatementProcessor {
-		void doQuery(Statement stmt) throws SQLException;
+		return ok("made user?");
 	}
 }
