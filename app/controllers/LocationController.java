88b35be589ea637fb9ee3fae73eed3010eb060c9
Felix Törnqvist <felix.trnqvist@gmail.com>
2017-05-08 14:50:31 +0200

    search returns all data (columns) from sql database

--- app/controllers/LocationController.java
+++ app/controllers/LocationController.java
@@ -9,93 +9,106 @@ import javax.inject.Inject;
 import java.sql.*;
 
 public class LocationController extends Controller {
-    private Database db;
+	private Database db;
 
-    @Inject
-    public LocationController(Database db) {
-        this.db = db;
-    }
+	@Inject
+	public LocationController(Database db) {
+		this.db = db;
+	}
 
-    public Result listLocations() {
-        final String[] result = {"["};
+	public Result listLocations() {
+		final String[] result = {"["};
 
-        SQLTools.StatementFiller sf = stmt -> {};
-        SQLTools.ResultSetProcesser rp = rs -> {
-            while(rs.next()){
-                int id  = rs.getInt("location_id");
-                String name_short = rs.getString("name_short");
-                int loc_type = rs.getInt("location_type");
-                double x = rs.getDouble("position_x");
-                double y = rs.getDouble("position_y");
+		SQLTools.StatementFiller sf = stmt -> {
+		};
+		SQLTools.ResultSetProcesser rp = rs -> {
+			while (rs.next()) {
+				int id = rs.getInt("location_id");
+				String name_short = rs.getString("name_short");
+				int loc_type = rs.getInt("location_type");
+				double x = rs.getDouble("position_x");
+				double y = rs.getDouble("position_y");
 
-                result[0] += "{ \"location_id\":\""+id+"\" \"name_short\":\""+name_short+"\" \"location_type\":\""+loc_type+"\" \"position_x\":\""+x+"\" \"location_y\":\""+y+"\", \n";
-            }
-            result[0] += "]";
-        };
+				result[0] += "{ \"location_id\":\"" + id + "\" \"name_short\":\"" + name_short + "\" \"location_type\":\"" + loc_type + "\" \"position_x\":\"" + x + "\" \"location_y\":\"" + y + "\", \n";
+			}
+			result[0] += "]";
+		};
 
-        try{
-            SQLTools.doPreparedStatement(db, "SELECT * FROM Locations", sf, rp);
-        } catch (SQLException e) {
-            return ok("couldn't list locations");
-        }
+		try {
+			SQLTools.doPreparedStatement(db, "SELECT * FROM Locations", sf, rp);
+		} catch (SQLException e) {
+			return ok("couldn't list locations");
+		}
 
-        return ok(result[0]);
-    }
+		return ok(result[0]);
+	}
 
-    public Result getLocation(int id){
-        final String[] result = {"["};
-        String id2 = ""+id;
+	public Result getLocation(int id) {
+		final String[] result = {"["};
+		String id2 = "" + id;
 
-        SQLTools.StatementFiller sf = stmt -> {
-            stmt.setString(1, id2);
-        };
-        SQLTools.ResultSetProcesser rp = rs -> {
-            while (rs.next()){
-                int locationId  = rs.getInt("location_id");
-                String name_short = rs.getString("name_short");
-                int loc_type = rs.getInt("location_type");
-                double x = rs.getDouble("position_x");
-                double y = rs.getDouble("position_y");
+		SQLTools.StatementFiller sf = stmt -> {
+			stmt.setString(1, id2);
+		};
+		SQLTools.ResultSetProcesser rp = rs -> {
+			while (rs.next()) {
+				int locationId = rs.getInt("location_id");
+				String name_short = rs.getString("name_short");
+				int loc_type = rs.getInt("location_type");
+				double x = rs.getDouble("position_x");
+				double y = rs.getDouble("position_y");
 
-                result[0] += "{ \"location_id\":\""+locationId+"\" \"name_short\":\""+name_short+"\" \"location_type\":\""+loc_type+"\" \"position_x\":\""+x+"\" \"location_y\":\""+y+"\", \n";
-            }
-            result[0] += "]";
-        };
+				result[0] += "{ \"location_id\":\"" + locationId + "\" \"name_short\":\"" + name_short + "\" \"location_type\":\"" + loc_type + "\" \"position_x\":\"" + x + "\" \"location_y\":\"" + y + "\", \n";
+			}
+			result[0] += "]";
+		};
 
-        try{
-            SQLTools.doPreparedStatement(db, "SELECT * FROM Locations WHERE location_id=?", sf, rp);
-        }catch(SQLException e){
-            return ok("couldn't load location");
-        }
+		try {
+			SQLTools.doPreparedStatement(db, "SELECT * FROM Locations WHERE location_id=?", sf, rp);
+		} catch (SQLException e) {
+			return ok("couldn't load location");
+		}
 
-        return ok(result[0]);
-    }
+		return ok(result[0]);
+	}
 
-    public Result searchLocations(String search){
-        final String[] result = {"["};
-        search += "%";
-        final String search2 = search;
+	public Result searchLocations(String search) {
+		final String[] result = {"["};
+		search += "%";
+		final String search2 = search;
 
-        SQLTools.StatementFiller sf = stmt -> {
-            stmt.setString(1, search2);
-        };
-        SQLTools.ResultSetProcesser rp = rs -> {
-            while (rs.next() && !rs.isLast()){
-                String name = rs.getString("name");
-                result[0] += "{ \"name\":\""+name+"\"  }, \n";
-            }
-            String name = rs.getString("name");
-            result[0] += "{ \"name\":\""+name+"\"  } ]";
-        };
+		SQLTools.StatementFiller sf = stmt -> {
+			stmt.setString(1, search2);
+		};
+		SQLTools.ResultSetProcesser rp = rs -> {
+			ResultSetMetaData metaData = rs.getMetaData();
 
-        try{
-            SQLTools.doPreparedStatement(db, "SELECT name FROM Locations WHERE name LIKE ?", sf, rp);
-        }catch(SQLException e){
-            return ok("couldn't load search");
-        }
+			String[] columns = new String[metaData.getColumnCount()];
+			for (int i = 0; i < columns.length; i++) {
+				columns[i] = metaData.getColumnName(i+1);
+			}
 
-        return ok(result[0]);
-    }
+			while (rs.next() && !rs.isLast()) {
+				result[0] += "{ ";
+
+				for (int i=0; i < columns.length; i++) {
+					result[0] += " \"" + columns[i] + "\": \"" + rs.getString(columns[i]) + "\", ";
+				}
+
+				result[0] += "  }, \n";
+			}
+			String name = rs.getString("name");
+			result[0] += "{ \"name\":\"" + name + "\"  } ]";
+		};
+
+		try {
+			SQLTools.doPreparedStatement(db, "SELECT * FROM Locations WHERE name LIKE ?", sf, rp);
+		} catch (SQLException e) {
+			return ok("couldn't load search");
+		}
+
+		return ok(result[0]);
+	}
 
 }
 
