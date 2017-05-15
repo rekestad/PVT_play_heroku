289ca97b77152130759a65aa7c8562d065ea5e2c
Filip Hörnsten <filip.hornsten@hotmail.com>
2017-05-15 15:35:28 +0200

    Added deleteChild method to UserController class for deleting a profile's children Added POST call for method in routes file

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -175,6 +175,28 @@ public class UserController extends Controller {
 		return ok(result[0]);
 	}
 
+	// DELETE LIKE
+	public Result deleteChild(){
+		JsonNode jNode = request().body().asJson();
+		System.out.println("test deleteChild");
+		System.out.println(jNode);
+
+		SQLTools.StatementFiller sf = pstmt -> {
+			pstmt.setLong(1, jNode.findPath("parent_id").asLong());
+			pstmt.setLong(2, jNode.findPath("child_id").asLong());
+		};
+
+		SQLTools.ResultSetProcessor rp = rs -> {};
+
+		try{
+			SQLTools.doPreparedStatement(db, "DELETE FROM User_children WHERE parent_id = ? AND child_id = ?", sf, rp);
+		} catch(SQLException e){
+			return internalServerError("couldn't delete child" + e);
+		}
+
+		return ok("deleted child");
+	}
+
 	// GET USERS AMOUNT OF LIKES
 	public Result getAmountOfLikes(long fbID) {
 		final JsonNode[] result = {null};
