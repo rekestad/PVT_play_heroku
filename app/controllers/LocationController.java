ecde318ceb36bc54f4adc4c0c9ef12e85f52c8af
Alexander Rekestad <alexander.rekestad@gmail.com>
2017-05-17 13:45:19 +0200

    getLocations hämtar numera events också

--- app/controllers/LocationController.java
+++ app/controllers/LocationController.java
@@ -12,31 +12,31 @@ import java.util.ArrayList;
 
 public class LocationController extends Controller {
 	private Database db;
-	private ArrayList<JsonNode> returnArray;
+//	private ArrayList<JsonNode> returnArray;
 
 	@Inject
 	public LocationController(Database db) {
 		this.db = db;
-		returnArray = new ArrayList<JsonNode>();
+//		returnArray = new ArrayList<JsonNode>();
 	}
 	
-	private void addReturnData(JsonNode jn) {
-		returnArray.add(jn);
-	}
-
-	private String getReturnData() {
-		String returnData = "";
-
-		for(JsonNode jn : returnArray)
-			returnData += jn;
-		returnArray.clear();
-
-		//returnData.replaceAll("%][%", ",");
-
-		returnData = returnData.replace("][",",");
-
-		return returnData;
-	}
+//	private void addReturnData(JsonNode jn) {
+//		returnArray.add(jn);
+//	}
+//
+//	private String getReturnData() {
+//		String returnData = "";
+//
+//		for(JsonNode jn : returnArray)
+//			returnData += jn;
+//		returnArray.clear();
+//
+//		//returnData.replaceAll("%][%", ",");
+//
+//		returnData = returnData.replace("][",",");
+//
+//		return returnData;
+//	}
 
 	public Result listLocations() {
 		final JsonNode[] result = {null};
@@ -54,46 +54,51 @@ public class LocationController extends Controller {
 
 	public Result getLocation(int id) {
 		final JsonNode[] result = {null};
-		String id2 = "" + id;
+		String sql = "SELECT l.location_id, lp.type_name, l.name, l.name_short, l.position_x, l.position_y, " +
+				"e.event_id, e.date, e.start_time, e.end_time, e.description, " +
+				"(SELECT Count(event_id) FROM Event_attendees WHERE event_id = e.event_id) AS attendees " +
+				"FROM Locations l, Events e, Location_types lp " +
+				"WHERE l.location_id = ? AND e.location_id = l.location_id AND l.location_type = lp.type_id";
 
 		SQLTools.StatementFiller sf = stmt -> {
-			stmt.setString(1, id2);
+			stmt.setInt(1, id);
 		};
+
 		SQLTools.ResultSetProcessor rp = rs -> {
 			result[0] = SQLTools.columnsAndRowsToJSON(rs);
 		};
 
 		try {
-			SQLTools.doPreparedStatement(db, "SELECT * FROM Locations WHERE location_id=?", sf, rp);
+			SQLTools.doPreparedStatement(db, sql, sf, rp);
 		} catch (SQLException e) {
-			return internalServerError("couldn't load location");
+			return internalServerError("Error: " + e.toString());
 		}
 
 		return ok(result[0]);
 	}
 
-	public Result getLocationAndEvents(int locationId) {
-		final JsonNode[] result = {null};
-		String sqlLocation = "SELECT * FROM Locations WHERE location_id=?";
-		String sqlEvents = "SELECT e.*, (SELECT Count(event_id) FROM Event_attendees WHERE event_id = e.event_id) AS NumberOfAttendees FROM Events e WHERE e.location_id = ?";
-
-		SQLTools.StatementFiller sf = stmt -> {
-			stmt.setInt(1, locationId);
-		};
-
-		SQLTools.ResultSetProcessor rp = rs -> {
-			addReturnData(SQLTools.columnsAndRowsToJSON(rs));
-		};
-
-		try {
-			SQLTools.doPreparedStatement(db, sqlLocation, sf, rp);
-			SQLTools.doPreparedStatement(db, sqlEvents, sf, rp);
-		} catch (SQLException e) {
-			return internalServerError("Error: " + e.toString());
-		}
-
-		return ok(getReturnData());
-	}
+//	public Result getLocationAndEvents(int locationId) {
+//		final JsonNode[] result = {null};
+//		String sqlLocation = "SELECT * FROM Locations WHERE location_id=?";
+//		String sqlEvents = "SELECT e.*, (SELECT Count(event_id) FROM Event_attendees WHERE event_id = e.event_id) AS NumberOfAttendees FROM Events e WHERE e.location_id = ?";
+//
+//		SQLTools.StatementFiller sf = stmt -> {
+//			stmt.setInt(1, locationId);
+//		};
+//
+//		SQLTools.ResultSetProcessor rp = rs -> {
+//			addReturnData(SQLTools.columnsAndRowsToJSON(rs));
+//		};
+//
+//		try {
+//			SQLTools.doPreparedStatement(db, sqlLocation, sf, rp);
+//			SQLTools.doPreparedStatement(db, sqlEvents, sf, rp);
+//		} catch (SQLException e) {
+//			return internalServerError("Error: " + e.toString());
+//		}
+//
+//		return ok(getReturnData());
+//	}
 
 	public Result searchLocations(String search) {
 		final JsonNode[] result = {null};
