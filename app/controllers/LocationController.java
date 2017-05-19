8bdaaca35199445b84f40ed62236a15101d2bf4d
Alexander Rekestad <alexander.rekestad@gmail.com>
2017-05-19 15:23:14 +0200

    La till funkis för att kolla om en användare likeat en plats

--- app/controllers/LocationController.java
+++ app/controllers/LocationController.java
@@ -10,70 +10,95 @@ import javax.inject.Inject;
 import java.sql.SQLException;
 
 public class LocationController extends Controller {
-	private Database db;
+    private Database db;
 
-	@Inject
-	public LocationController(Database db) {
-		this.db = db;
-	}
+    @Inject
+    public LocationController(Database db) {
+        this.db = db;
+    }
 
-	public Result listLocations() {
-		final JsonNode[] result = {null};
+    public Result listLocations() {
+        final JsonNode[] result = {null};
 
-		SQLTools.ResultSetProcessor rp = rs -> result[0] = SQLTools.columnsAndRowsToJSON(rs);
+        SQLTools.ResultSetProcessor rp = rs -> result[0] = SQLTools.columnsAndRowsToJSON(rs);
 
-		try {
-			SQLTools.doPreparedStatement(db, "SELECT * FROM Locations", stmt -> {}, rp);
-		} catch (SQLException e) {
-			return internalServerError("couldn't list locations");
-		}
+        try {
+            SQLTools.doPreparedStatement(db, "SELECT * FROM Locations", stmt -> {
+            }, rp);
+        } catch (SQLException e) {
+            return internalServerError("couldn't list locations");
+        }
 
-		return ok(result[0]);
-	}
+        return ok(result[0]);
+    }
 
-	public Result getLocation(int locationId) {
-		final JsonNode[] result = {null};
-		String sql = "SELECT * FROM Locations WHERE location_id = ?";
+    public Result getLocation(int locationId) {
+        final JsonNode[] result = {null};
+        String sql = "SELECT * FROM Locations WHERE location_id = ?";
 
-		SQLTools.StatementFiller sf = stmt -> {
-			stmt.setInt(1, locationId);
-		};
+        SQLTools.StatementFiller sf = stmt -> {
+            stmt.setInt(1, locationId);
+        };
 
-		SQLTools.ResultSetProcessor rp = rs -> {
-			result[0] = SQLTools.columnsAndRowsToJSON(rs);
-		};
+        SQLTools.ResultSetProcessor rp = rs -> {
+            result[0] = SQLTools.columnsAndRowsToJSON(rs);
+        };
 
-		try {
-			SQLTools.doPreparedStatement(db, sql, sf, rp);
-		} catch (SQLException e) {
-			return internalServerError("Error: " + e.toString());
-		}
+        try {
+            SQLTools.doPreparedStatement(db, sql, sf, rp);
+        } catch (SQLException e) {
+            return internalServerError("Error: " + e.toString());
+        }
 
-		return ok(result[0]);
-	}
+        return ok(result[0]);
+    }
 
-	public Result searchLocations(String search) {
-		final JsonNode[] result = {null};
-		//search += "%";
-		final String search2 = search;
+    public Result searchLocations(String search) {
+        final JsonNode[] result = {null};
+        //search += "%";
+        final String search2 = search;
 
-		SQLTools.StatementFiller sf = stmt -> {
-			stmt.setString(1, search2);
-		};
-		
-		SQLTools.ResultSetProcessor rp = rs -> {
-			result[0] = SQLTools.columnsAndRowsToJSON(rs);
-		};
+        SQLTools.StatementFiller sf = stmt -> {
+            stmt.setString(1, search2);
+        };
 
-		try {
-			SQLTools.doPreparedStatement(db, "SELECT * FROM Locations WHERE name REGEXP ?", sf, rp);
-		} catch (SQLException e) {
-			return internalServerError("couldn't load search");
-		}
+        SQLTools.ResultSetProcessor rp = rs -> {
+            result[0] = SQLTools.columnsAndRowsToJSON(rs);
+        };
+
+        try {
+            SQLTools.doPreparedStatement(db, "SELECT * FROM Locations WHERE name REGEXP ?", sf, rp);
+        } catch (SQLException e) {
+            return internalServerError("couldn't load search");
+        }
 
 
-		return ok(result[0]);
-	}
+        return ok(result[0]);
+    }
+
+    // CHECK IF LOCATION IS USER FAVOURITE
+    public Result checkIfFavourite(long userId, int locationId) {
+        final JsonNode[] result = {null};
+        String sql = "SELECT IF(EXISTS(SELECT * FROM User_locations WHERE user_id = ? AND location_id = ?),1,0) AS result";
+
+        SQLTools.StatementFiller sf = stmt -> {
+            stmt.setLong(1, userId);
+            stmt.setInt(2, locationId);
+
+        };
+
+        SQLTools.ResultSetProcessor rp = rs -> {
+            result[0] = SQLTools.columnsAndRowsToJSON(rs);
+        };
+
+        try {
+            SQLTools.doPreparedStatement(db, sql, sf, rp);
+        } catch (SQLException e) {
+            return internalServerError("Error: " + e.toString());
+        }
+
+        return ok(result[0]);
+    }
 }
 
 
